de48f395386e6cee8ea2776059b7549930ab2b2e
정유선 <59593174+jys96@users.noreply.github.com>
2026-01-27 15:49:51 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
@@ -1,44 +1,30 @@
-def ch_in(wallet, bill):
-    if wallet >= bill:
-        return True
-    else:
-        return False
+def check_bill(wallet, bill):
+    result = False
+    
+    
+    if (max(wallet) < max(bill)) or (min(wallet) < min(bill)):
+        result = True
+    
+    return result
 
 def half_bill(bill):
     if bill[0] > bill[1]:
         bill[0] = bill[0] // 2
-    else:
+    elif bill[0] < bill[1]:
         bill[1] = bill[1] // 2
-
+    
     return bill
 
-def rotation_bill(wallet, bill):
-    result = []
-    for i in range(2):
-        result.append(ch_in(wallet[i], bill[i]))
-
-    if False in result:
-        result = []
-        result.append(ch_in(wallet[0], bill[1]))
-        result.append(ch_in(wallet[1], bill[0]))
-
-        if False in result:
-            return False
-        else:
-            return True
-    else:
-        return True
+def translate_bill(bill):
+    
+    return [bill[1], bill[0]]
 
 def solution(wallet, bill):
     answer = 0
-
-    while True:
-        result = rotation_bill(wallet, bill)
-
-        if result == True:
-            break
-        else:
-            bill = half_bill(bill)
-            answer += 1
-
+    
+    while check_bill(wallet, bill):
+        bill = half_bill(bill)
+        bill = translate_bill(bill)
+        answer += 1
+        
     return answer
